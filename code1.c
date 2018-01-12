6af69a1753a87196cbfa8381a53812d5ce2f0f17
AnubhiSaxena <anubhisaxena1997@gmail.com>
2018-01-12 22:48:13 +0530

    Create code1.c

---
+++ code1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter no");
+scanf("%d",&n);
+if(n>0)
+{
+printf("positive no");
+}
+else if(n<0)
+{
+printf("negative no");
+}
+else
+{
+printf("zero");
+}
+}
