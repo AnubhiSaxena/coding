63dd33d23c4fbf36538c7a3f56566332820750aa
AnubhiSaxena <anubhisaxena1997@gmail.com>
2018-01-15 23:20:06 +0530

    Create code24.c

---
+++ code24.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+{
+int n;
+int arr[15],temp=0;
+scanf("%d",&n);
+for(int i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+for(int i=0;i<n;i++)
+{
+for(int j=i+1;j<n;j++)
+{
+if(arr[i]>arr[j])			
+{
+temp=arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+}
+}
+}
+for(int i=0;i<n;i++)
+{
+printf("%d ",arr[i]);
+}
+}
